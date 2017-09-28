af911950de155399292ce5461de4d044d51fab04
Chase Qi <qiziming1@gmail.com>
2017-09-28 15:49:44 -0400

    Added log in screen

--- app/src/main/java/com/example/zqi4525/leaguecompanion/SplashActivity.java
+++ app/src/main/java/com/example/zqi4525/leaguecompanion/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, HomeScreen.class);
+        Intent intent = new Intent(this, LogInActivity.class);
         startActivity(intent);
         finish();
     }
