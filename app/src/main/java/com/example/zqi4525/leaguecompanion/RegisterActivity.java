62ac3c4b140a9b5d44cb0f96976ef9fa31820080
Chase Qi <qiziming1@gmail.com>
2017-10-04 12:48:23 -0400

    Take IGN and display a toast

--- app/src/main/java/com/example/zqi4525/leaguecompanion/RegisterActivity.java
+++ app/src/main/java/com/example/zqi4525/leaguecompanion/RegisterActivity.java
@@ -1,13 +1,38 @@
 package com.example.zqi4525.leaguecompanion;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText ign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        ign = (EditText)findViewById(R.id.ignText);
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                            Context context = getApplicationContext();
+                            CharSequence text = ign.getText().toString()+" successfully registered.";
+                            int duration = Toast.LENGTH_SHORT;
+
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                            Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
+                            startActivity(intent);
+                    }
+                }
+        );
     }
 }
